48f87001644f439f3e2d3eb455e7f418a3e23705
Alexzen90 <aporteron90@gmail.com>
2024-04-09 15:46:22 +0200

    Update sur les valeurs

--- geometry.js
+++ geometry.js
@@ -13,5 +13,21 @@ rectangle.diagonale = Math.sqrt(rectangle.Largeur**2 + rectangle.Longueur**2)
 
 rectangle.diagonale = Number(rectangle.diagonale.toFixed(2))
 
-console.log("\nx:", rectangle.x, rectangle.unite,"\n"+"y:", rectangle.y, rectangle.unite,"\n"+"Perimetre:", rectangle.perimetre, rectangle.unite,"\n"+"Aire:", rectangle.area, rectangle.unite +2,"\n"+"Diagonal:", rectangle.diagonale, rectangle.unite, "\n")
+//console.log("\nx:", rectangle.x, rectangle.unite,"\n"+"y:", rectangle.y, rectangle.unite,"\n"+"Perimetre:", rectangle.perimetre, rectangle.unite,"\n"+"Aire:", rectangle.area, rectangle.unite +2,"\n"+"Diagonal:", rectangle.diagonale, rectangle.unite, "\n")
 
+let variableSimple1 = 12
+let variableSimple2 = variableSimple1
+variableSimple1 = 35
+
+console.log("variableSimple1", variableSimple1)
+console.log("variableSimple2", variableSimple2)
+
+let variableComplexe1 = ["informatique", "sport", "television"]
+let variableComplexe2 = variableComplexe1
+let variableComplexe3 = [...variableComplexe1]
+
+variableComplexe2.push("course")
+
+console.log("variableComplexe1", variableComplexe1)
+console.log("variableComplexe2", variableComplexe2)
+console.log("variableComplexe3", variableComplexe3)
