028e8f502c9b13483f32aa1185a5ea518dcba6df
Alexzen90 <aporteron90@gmail.com>
2024-04-09 09:13:19 +0200

    Exercice livres vendus par jour

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let message = "Hello world!"                          //Greetings
+let message = "Hello world!"                            //Greetings
 console.log(message)
-message = "Je suis sexy"                              //Unlike Lutfu
+message = "Je suis sexy"                                //Unlike Lutfu
 console.log(message)
 
 // Exercice 2 : Les opérateurs
 
-const soldeCompte = 1000                           //Solde du compte
-const salaire = 1200                                 //Mon salaire
-const soldeCompteApresSalaire = soldeCompte + salaire //Solde après salaire
+const soldeCompte = 1000                                //Solde du compte
+const salaire = 1200                                    //Mon salaire
+const soldeCompteApresSalaire = soldeCompte + salaire   //Solde après salaire
 console.log(soldeCompteApresSalaire)
 
 // Exercice 3 : Vente de pièces
 
-let piecesVendues = 1000                              //Nombre pièces vendues
-piecesVendues = piecesVendues + 10                    //Ajouter 10 pièces vendues
+let piecesVendues = 1000                                //Nombre pièces vendues
+piecesVendues = piecesVendues + 10                      //Ajouter 10 pièces vendues
 console.log(piecesVendues)
 
-piecesVendues += 10                                   //Plus rapide
+piecesVendues += 10                                     //Plus rapide
 console.log(piecesVendues)
 
 //Exercice 4
---
+++ moyenne_par_j_livre_vendu.js
@@ -0,0 +1,5 @@
+let livresVendus = 650
+livresVendus += 50
+livresVendusParJour = livresVendus / 7
+
+console.log("Le nombre de livres vendus par jour est " + livresVendusParJour)
