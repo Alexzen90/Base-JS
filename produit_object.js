2833fa4564daae4806485d6db981fe93389bbd96
Alexzen90 <aporteron90@gmail.com>
2024-04-09 11:44:08 +0200

    Exercice object

--- produit_object.js
+++ produit_object.js
@@ -12,3 +12,13 @@ produit.valeur_stock = produit.stock * produit.prix
 console.log(produit)
 
 console.log("Produit :", produit.nom, "/ Valeur du stock :", produit.valeur_stock, "/ Gain :", produit.gain)
+
+produit.cost = 12
+produit.total_cost = produit.cost * (produit.nombre_vendu + produit.stock)
+console.log("Somme d'achat des produits : ", produit.total_cost)
+
+let benefice_par_produit = produit.prix - produit.cost
+console.log("Bénéfice par produit : ", benefice_par_produit)
+
+let pourcentage_benefice = 100 * (benefice_par_produit/produit.prix)
+console.log("Pourcentage de bénéfice par produit :", pourcentage_benefice +" ~", Math.round(pourcentage_benefice) + " %")
